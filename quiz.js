bba60eae578fe6f649a1aa8621f7ef235275126a
Matt Moe <mattmoe@sbcglobal.net>
2020-08-15 12:23:09 -0500

    updated JS

--- quiz.js
+++ quiz.js
@@ -26,19 +26,28 @@ var questions = [
         answer: "quotes"
     },  
 ]
+// Starting var and const
+let timeLeft = 70
 const timeLeftDisplay = document.querySelector('#timer')
+const startingEl = document.querySelector(".rowOne")
 const questionElement = document.getElementById("question")
 const questionContainer = document.querySelector(".question-container")
-let timeLeft = 70
 var currentQuestionIndex = 0
+var questionTitle = document.getElementById('questionTitle')
+var gameOver = document.querySelector('.rowThree')
+var button1 = document.getElementById('button1')
+var button2 = document.getElementById('button2')
+var button3 = document.getElementById('button3')
+var button4 = document.getElementById('button4')
+var results = document.querySelector('#results')
 var score = 0;
-var button1 = document.getElementById("button1");
-var button2 = document.getElementById("button2");
-var button3 = document.getElementById("button3");
-var button4 = document.getElementById("button4");
 var endScore = document.querySelector('#endScore');
 var submitBtn = document.getElementById('submitBtn');
 
+document.addEventListener('DOMContentLoaded', () => {
+    const startBtn = document.querySelector('start-btn')
+
+    const startButton = document.getElementById("start-btn");
 
 // timer function
 function countDown() {
@@ -49,4 +58,53 @@ function countDown() {
         timeLeftDisplay.innerHTML = timeLeft
         timeLeft -= 1
     }, 1000)
+}
+// Quiz start
+function startGame() {
+    console.log('Start');
+    console.log(startingEl);
+    startingEl.style.display = "none";
+    questionContainer.style.display = "block";
+    setNextQuestion()
+}
+// Countdown
+startButton.addEventListener('click', function (){
+    startGame();
+    countDown();
+    })
+})
+function setNextQuestion() {
+    var question = questions[currentQuestionIndex]
+    button1.textContent = question.choices[0]
+    button2.textContent = question.choices[1]
+    button3.textContent = question.choices[2]
+    button4.textContent = question.choices[3]
+    questionTitle.textContent = question.title
+}
+// event listeners for answers
+button1.addEventListener('click', checkAnswer);
+button2.addEventListener('click', checkAnswer);
+button3.addEventListener('click', checkAnswer);
+button4.addEventListener('click', checkAnswer);
+//check to see answers
+function checkAnswer(event) {
+    var answer = questions[currentQuestionIndex].answer
+    // feeback if answer is correct
+    if (answer === event.target.innerHTML) {
+        feedBack.textContent = "Correct!";
+        score++;
+        clearFeedBack(); 
+    // feedback if answer is wrong
+    } else{
+        feedBack.textContent = "Sorry that is not correct";
+        clearFeedBack(); 
+        timeLeft-=15
+    }
+    currentQuestionIndex++
+    if (currentQuestionIndex < questions.length) {
+        setNextQuestion()  
+    }
+    else{
+        endGame();
+    }
 }
