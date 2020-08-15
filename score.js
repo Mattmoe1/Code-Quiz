26355cd6b780a4e1f5c2dae34f86cd6d591c8c53
Matt Moe <mattmoe@sbcglobal.net>
2020-08-15 14:02:24 -0500

    fixed the highscore page

--- score.js
+++ score.js
@@ -1,7 +1,7 @@
 function getScore() {
     var quizContent = `
     <h2>` + localStorage.getItem("highscoreName") + `'s highscore is: </h2>
-    <h1>` + localStorage.getItem("highscore") + `</h1><br>`;
+    <h1>` + localStorage.getItem("highscore") + `</h1>`;
 
     document.getElementById("quizGame").innerHTML = quizContent;
 }
